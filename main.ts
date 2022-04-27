306bf18e730edd38670fef5ca6ebd7cded2c371b
PatriciaPala <101339915+PatriciaPala@users.noreply.github.com>
2022-04-27 09:33:52 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(doSomething(Num1))
+})
+function doSomething (num: number) {
+    let Num = 0
+    return Num / 1
+}
+let Num1 = 0
+Num1 = 8
+basic.forever(function () {
+	
+})
